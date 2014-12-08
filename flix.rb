0ac3c556ac6ad83a0456afd0acfc338de2e49e54
AndrewSB <asbreckenridge@me.com>
2014-12-08 00:20:11 -0600

    add brew formula

---
+++ flix.rb
@@ -0,0 +1,10 @@
+require 'formula'
+
+class Flix < Formula
+  homepage 'http://andrewsb.github.io/flix'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/2.1.2/apache-cassandra-2.1.2-bin.tar.gz'
+  # sha1 '93d89de9aadc50acfee84b502daa3f96c9eca782'
+
+  def install
+    
+  end
