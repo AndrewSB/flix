dba69287dc82cade1f0b14c56083f703d974d432
AndrewSB <asbreckenridge@me.com>
2014-12-07 22:46:35 -0600

    finish string sanitization

--- flix.sh
+++ flix.sh
@@ -4,10 +4,11 @@
 
 if [ $1 ]
 	then 
-	NAME=$1
-	echo $NAME
-	echo "calling peerflix"
-	curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://torrentproject.se/?s=ubuntu+dvd&out=json
+	SANITIZED=`echo $* | tr -d ' '`
+	echo $SANITIZED
+	# echo "calling peerflix"
+	# URL="http://torrentproject.se/?s="$INPUT"&out=json"
+	# curl -H GET $URL
 else
 	echo "flix - A simple convience search for peerflix"
 	echo ""
